447bfbe980889e71f11efc7d62853bacf42131ee
MazaFaaka <haitovvasyl@gmail.com>
2025-06-15 23:05:23 +0200

    compled generics and homework ts 1

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,3 +1,3 @@
-function merge(objA, objB) {
-  return Object.assign(objA, objB);
+function merge<T extends object, B extends object>(objA: T, objB: B): T & B {
+  return Object.assign({}, objA, objB);
 }
--- src/generics/5.ts
+++ src/generics/5.ts
@@ -4,8 +4,10 @@ export enum UserRole {
   guest = "guest",
 }
 
+type UsersRole = Record<UserRole, string>;
+
 // Замініть наступний код на версію за допомогою Record
-const RoleDescription = {
+const RoleDescription: UsersRole = {
   admin: "Admin User",
   editor: "Editor User",
   guest: "Guest User",
